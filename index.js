36d82f1303f76ad846f39a427d8df75073865970
Gwenstard <gwen.kerbastard@gmail.com>
2024-06-10 19:30:57 +0300

    Positioning of the character on the map

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 //instead of declaring our image on html, we did it on JS this way
 const image = new Image();
 //forgot the . in the path that's why it didn't work
-image.src = './images/pokemonMap.png'
-console.log(image);
+image.src = './images/pokemonMap.png';
+
+const playerImage = new Image();
+playerImage.src = './images/playerDown.png'
 
 //arrow function
 image.onload = () => {
-    c.drawImage(image, -1100, -500);
+    c.drawImage(image, -1170, -550)
+    c.drawImage(playerImage,
+        //croping arguments
+        0,
+        0,
+        playerImage.width/4,
+        playerImage.height,
+        //position arguments
+        (canvas.width/2) - (playerImage.width/4)/2,
+        (canvas.height/2)-(playerImage.height/2 ),
+        playerImage.width/4,
+        playerImage.height,
+    )
 }
