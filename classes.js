ff019b9a471a15f71876fb449d965a5c376a29e0
Gwendal Kerbastard <gwen.kerbastard@gmail.com>
2024-08-01 00:18:35 +0300

    Export de 2 classes pour clean le code

--- classes.js
+++ classes.js
@@ -37,3 +37,18 @@ class Sprite {
     }          
     }
 }
+
+class Frontiere {
+    static WIDTH = 48
+    static HEIGHT = 48
+    constructor({position}) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw() {
+        c.fillStyle = 'rgba(255,0,0,0.5)'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
